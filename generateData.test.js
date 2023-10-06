88b29938a1292dd7e120726ae7711c28dfc0bf73
jackhclee <38123458+jackhclee@users.noreply.github.com>
2023-10-06 23:41:35 +0100

    workflow

--- generateData.test.js
+++ generateData.test.js
@@ -19,3 +19,8 @@ test('should have 100 volunteers in DB', async () => {
   let result = await db.query({text: "SELECT count(*) from volunteer"})
   expect(parseInt(result.rows[0].count)).toBe(100);
 });
+
+test('should have 100 volunteers in DB', async () => {
+    let result = await db.query({text: "SELECT count(*) from volunteer"})
+    expect(parseInt(result.rows[0].count)).toBe(100);
+  });
